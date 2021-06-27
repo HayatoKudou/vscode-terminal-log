d1829ae141ee6e3ae51e8f1e770042d3f8dd5495
kudou <kudoh115@gmail.com>
2021-06-28 00:29:14 +0900

    master

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 import * as vscode from 'vscode';
 
 import { DepNodeProvider } from './nodeDependencies';
-import { FtpExplorer } from './ftpExplorer';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -11,5 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
 	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 
-	new FtpExplorer(context);
+	vscode.window.createTreeView('nodeDependencies', {
+		treeDataProvider: nodeDependenciesProvider
+	});
 }
